8f02f311ee3d8c1f0542463968701fa2073b58c8
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-03-13 18:17:37 -0400

    chore: first interaction

---
+++ first.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# This is a comment!
+echo "Hello  World "       # Hello World
+echo "Hello World"         # Hello World
+echo "Hello * World"       # Hello * World
+echo Hello * World         # Hello * World
+echo Hello      World      # Hello World
+echo "Hello" World         # Hello World
+echo Hello "      " World  # Hello World
+echo "Hello "*" World"     # Hello * World
+echo `hello` world         # hello world
+echo 'hello' world         # hello world
